8948c10ede116ca423d147f5e00fb461381cb090
Brett Henderson <bretth18@gmail.com>
2015-09-21 12:12:12 -0700

    Initial Commit

---
+++ VCCWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VCCWatch
+//
+//  Created by Brett Henderson on 9/21/15.
+//  Copyright © 2015 Brett Henderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
